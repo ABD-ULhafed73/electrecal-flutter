a3b07357fd47d5842874ec96c82b9d55cf5fc739
ABD-ULhafed73 <abdhafed73@gmail.co>
2024-05-30 15:19:15 +0300

    electrical second

---
+++ lib/models/cars.dart
@@ -0,0 +1,16 @@
+class campanycarsmodael {
+  final int id;
+  final String name, image;
+
+  campanycarsmodael(
+      {this.id,
+        this.name,
+        this.image
+      });
+}
+List<campanycarsmodael> campany_cars =
+[
+  campanycarsmodael(id: 1,image: 'images/car.png',name: 'تويوتا '),
+  campanycarsmodael(id:2,image: 'images/car.png',name: 'نيسان'),
+  campanycarsmodael(id: 3,image: 'images/car.png',name: 'فورد'),
+];
